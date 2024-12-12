e2124cad51b7ee7f03c37bc672c1af4dc8e8b058
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2024-12-11 19:11:45 -0500

    Staging Changes for Andrew I have been working on refactoring the Mission Blue API to pass the Pylint linter. I am pushing up the work that I have done so far.

--- mission_blue.py
+++ mission_blue.py
@@ -1,7 +1,8 @@
+import os
+import sys
 import requests
 import pandas as pd
 from dotenv import load_dotenv
-import os
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -24,20 +25,43 @@ def create_session():
     payload = {"identifier": BLUESKY_HANDLE, "password": BLUESKY_APP_PASSWORD}
 
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(url, json=payload, timeout=10)
         response.raise_for_status()
         session = response.json()
         return session["accessJwt"]
-    except requests.exceptions.RequestException as e:
-        print("Error during authentication:", e)
+    except requests.exceptions.RequestException as err:
+        print("Error during authentication:", err)
         print("Response:", response.text if "response" in locals() else "No response")
-        exit(1)
+        sys.exit(1)
 
 
-# Figure Out
-def search_posts(
-    query, access_token, since, until, limit=25, sort="top", cursor="", posts=[]
+def generate_query_params(
+    query: str, since: str, until: str, limit=25, sort="top", cursor=""
 ):
+    """This functions
+
+    Args:
+        query (str): The search term for the BlueSky Posts
+        since (str): The start date for posts
+        until (str): The end date for posts
+        limit (int, optional): Number of posts queried. Max is 100.
+        sort (str): Query for top posts or latest post
+        cursor (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+    return {
+        "q": query,
+        "sort": sort,
+        "since": since,
+        "until": until,
+        "limit": limit,
+        "cursor": cursor,
+    }
+
+
+def search_posts(params, token):
     """
     Search for posts using the BlueSky API.
 
@@ -47,24 +71,17 @@ def search_posts(
     :param sort: Sorting order ('latest' or 'top').
     :return: List of posts.
     """
+    posts = []
     url = f"{BASE_URL}/app.bsky.feed.searchPosts"
     headers = {
-        "Authorization": f"Bearer {access_token}",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
-    }
-    params = {
-        "q": query,
-        "sort": sort,
-        "since": since,
-        "until": until,
-        "limit": limit,
-        "cursor": cursor,
     }
     counter = 0
     while True:
         counter += 1
         try:
-            response = requests.get(url, headers=headers, params=params)
+            response = requests.get(url, headers=headers, params=params, timeout=10)
             # print(response)
             response.raise_for_status()
             posts = response.json().get("posts", []) + posts
@@ -75,12 +92,12 @@ def search_posts(
                 return posts
 
             params["cursor"] = next_cursor
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching posts: {e}")
+        except requests.exceptions.RequestException as err:
+            print(f"Error fetching posts: {err}")
             print(
                 "Response:", response.text if "response" in locals() else "No response"
             )
-            return []
+            return posts
 
 
 def extract_post_data(posts):
@@ -88,14 +105,11 @@ def extract_post_data(posts):
     Extract relevant data from posts and filter by date range.
 
     :param posts: List of raw posts.
-    :param start_date: Start date for filtering (YYYY-MM-DD).
-    :param end_date: End date for filtering (YYYY-MM-DD).
+    :param START_DATE: Start date for filtering (YYYY-MM-DD).
+    :param END_DATE: End date for filtering (YYYY-MM-DD).
     :return: List of dictionaries containing post data.
     """
     extracted_data = []
-    # if start_date and end_date:
-    #     start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    #     end_date = datetime.strptime(end_date, "%Y-%m-%d")
 
     for post in posts:
         try:
@@ -104,38 +118,34 @@ def extract_post_data(posts):
             created_at = post["indexedAt"]  # Extract the indexed timestamp
             # cid = post["cid"]
             # keys = post.keys()
-            postId = post["uri"].split("/")[-1]
-            postLink = f"https://bsky.app/profile/{author_handle}/post/{postId}"
+            post_id = post["uri"].split("/")[-1]
+            post_link = f"https://bsky.app/profile/{author_handle}/post/{post_id}"
 
-            # Filter by date range
-            # if start_date <= created_at_date <= end_date:
             extracted_data.append(
                 {
                     "author": author_handle,
                     "content": post_content,
                     "created_at": created_at,
-                    # "cid": cid,
-                    # "keys": keys,
-                    "postLink": postLink,
+                    "post_link": post_link,
                 }
             )
-        except KeyError as e:
-            print(f"Missing data in post: {e}")
+        except KeyError as err:
+            print(f"Missing data in post: {err}")
     return extracted_data
 
 
-def save_to_csv(post_data, filename):
+def save_to_csv(data, filename):
     """
     Save post data to a CSV file.
 
     :param post_data: List of post data dictionaries.
     :param filename: Output CSV filename.
     """
-    if post_data:
+    if data:
         if os.path.isfile(filename):
             os.remove(filename)
-        df = pd.DataFrame(post_data)
-        df.to_csv(filename, index=False)
+        data_frame = pd.DataFrame(data)
+        data_frame.to_csv(filename, index=False)
         print(f"Data saved to {filename}")
     else:
         print("No posts to save.")
@@ -146,25 +156,20 @@ if __name__ == "__main__":
     sport_name = input("Enter the sport you're querying: ")
     search_query = "Olympics 2024 " + sport_name
     print(search_query)
-    start_date = "2024-06-28T00:00:00Z"
-    end_date = "2024-09-28T23:59:59Z"
+    START_DATE = "2024-06-28T00:00:00Z"
+    END_DATE = "2024-09-28T23:59:59Z"
+    SORT = "top"
 
     # Authenticate and create a session
     print("Authenticating...")
     access_token = create_session()
     print("Authentication successful.")
 
+    query_param = generate_query_params(search_query, START_DATE, END_DATE)
+
     # Fetch posts
     print("Fetching posts...")
-    raw_posts = search_posts(
-        search_query,
-        access_token,
-        start_date,
-        end_date,
-        limit=100,
-        sort="latest",
-        cursor="",
-    )  # Can change limit
+    raw_posts = search_posts(query_param, access_token)
 
     # Extract post data
     print("Extracting post data...")
