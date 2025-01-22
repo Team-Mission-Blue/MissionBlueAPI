75099ee85587d6956d688029c074cc2abbba8e88
andewmark <atmarkar@syr.edu>
2025-01-22 17:06:18 -0500

    Auth Test Development Just adding all updates so I can work on this on my desktop

---
+++ tests/auth_test.py
@@ -0,0 +1,13 @@
+"""
+    Testing suite for the mission_blue module.
+"""
+
+import os
+import unittest
+from auth import load_credentials
+
+class TestLoad_Credentials:
+    
+
+if __name__ == "__main__":
+    unittest.main()
