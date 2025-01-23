9cb447406e6a2fe09c7ee62eeed64ab7f0aa10cc
andewmark <atmarkar@syr.edu>
2025-01-22 20:16:36 -0500

    Auth_Test Development Created / continued to develop Auth_Test.py to test the load_credentials method Created mock .env files and variables for tests to read in

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -2,12 +2,49 @@
     Testing suite for the mission_blue module.
 """
 
-import os
+#pylint: disable=W0613
+#pylint: disable=C0301
+#pylint: disable=E0401
+
 import unittest
+from unittest.mock import patch
 from auth import load_credentials
 
-class TestLoad_Credentials:
-    
+class TestLoadCredentials(unittest.TestCase):
+    """
+    Testing the load_credentials method
+    """
+
+    @patch("auth.load_dotenv", return_value=False)
+    def test_no_env(self, mock_load_dotenv):
+        """
+        Test if .env file does not exist
+        """
+        with self.assertRaises(SystemExit) as cm:
+            load_credentials()
+        self.assertEqual(cm.exception.code, 1)
+
+        mock_load_dotenv.assert_called_once()
+
+    @patch("os.getenv", side_effect=lambda key: "" if key in ["BLUESKY_HANDLE", "BLUESKY_APP_PASSWORD"] else None)
+    @patch("auth.load_dotenv", return_value=True)
+    def test_env_with_no_credentials(self, mock_load_dotenv, mock_getenv):
+        """
+        Test if .env exist but username and password are empty
+        """
+        with self.assertRaises(AssertionError) as cm:
+            load_credentials()
+        self.assertIn("can not be empty", str(cm.exception))
+
+    @patch("auth.load_dotenv", return_value=True)
+    @patch("os.getenv", side_effect=lambda key: "any_value" if key in ["BLUESKY_HANDLE", "BLUESKY_APP_PASSWORD"] else None)
+    def test_env_exist_with_valid_credentials(self, mock_load_dotenv, mock_getenv):
+        """
+        Test if .env exists with valid username and password
+        """
+        credentials = load_credentials()
+        self.assertTrue(all(credentials))
+
 
 if __name__ == "__main__":
     unittest.main()
